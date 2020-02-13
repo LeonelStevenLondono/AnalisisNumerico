de125ee378fffc8aa2e01a5ae63c2359fb5e7a0b
LeonelStevenLondoño <48395420+LeonelStevenLondono@users.noreply.github.com>
2020-02-13 15:52:10 -0500

    Add files via upload

---
+++ Talleres/Taller1/Parte2/naturales.R
@@ -0,0 +1,31 @@
+
+#-------------------
+#autores:
+#Andres Gil 
+#Erika Gonzalez 
+#Leonel Londono
+#Analisis numerico
+
+#------------------
+sumaNaturales <- function(b)
+{
+  vecto <- c()
+  for( i in b)
+  {
+    suma = 0
+    
+    for(d in 0:i^2)
+    {
+      suma = suma + d^2
+    }
+    vecto = c(vecto,suma)
+  }
+  plot(b,vecto,xlab="n",ylab="suma de cuadradados")
+  lines(b, vecto, col = "blue")
+  
+  
+  
+}
+b<-c(10,20,30,35,40,50,60,70,80,90,100,200,300,400,500,600)
+
+sumaNaturales(b)
---
+++ Talleres/Taller1/Parte2/sumas por matriz.R
@@ -0,0 +1,45 @@
+#-------------------
+#autores:
+#Andres Gil 
+#Erika Gonzalez 
+#Leonel Londono
+#Analisis numerico
+
+#------------------
+
+matrizTriang<-function(b)
+{
+  sumas <-c()
+  for(z in b)
+  {
+    
+    cont = 0  
+    matriz<-matrix(0,ncol=z,nrow = z)
+    for(i in 1:z )
+    {
+      for (j in 1:i)
+      {
+      if(j==z)
+      {
+        matriz[i,j]=1
+      }
+        else
+        {
+            
+        matriz[i,j]=1
+        }
+        
+        }
+      cont=cont+1
+    }
+sumaf=apply(matriz,1,sum)
+value=sum(sumaf)
+sumas = c(sumas,value)
+  }
+  
+plot(b,sumas,xlab="Tamaño matriz",ylab="Sumas por matriz")
+lines(b,sumas, col = "blue")
+}
+b<-c(10,20,30,35,40,50,60,70,80,90)
+matrizTriang(b)
+
